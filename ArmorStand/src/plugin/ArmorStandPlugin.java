e2067f078cb26c4df8f46d0343a9a5f9206865b0
Jorge <jorgehuertatoledo@gmail.com>
2015-04-11 10:09:11 +0200

    EspadaCheta

--- ArmorStand/src/plugin/ArmorStandPlugin.java
+++ ArmorStand/src/plugin/ArmorStandPlugin.java
@@ -5,11 +5,13 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.NPC;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
@@ -30,62 +32,65 @@ public class ArmorStandPlugin extends JavaPlugin implements Listener{
 	public void onEnable() {
 		Bukkit.getServer().getPluginManager().registerEvents(this,this);
 	}
-    
-	@Override
-	public boolean onCommand(CommandSender sender, Command command,String label, String[] args) {
-		if(command.getName().equalsIgnoreCase("armor")) {
-			 p = ((Player)sender);
-			Location l = p.getLocation();
-			l.add(0, -1, 0);
-			final ArmorStand a = p.getWorld().spawn(l, ArmorStand.class);
-			a.setGravity(false);
+	@EventHandler
+	public void onPlayerMovea(PlayerMoveEvent e){
+		p = e.getPlayer();
 		
-			a.setBodyPose(new EulerAngle(1,0,0));
-			a.setArms(true);
-			a.setVisible(false);
-			a.setChestplate(new ItemStack(Material.CHAINMAIL_CHESTPLATE));
-			ItemStack skull = new ItemStack(Material.SKULL_ITEM,1,(short)3);
-			SkullMeta im = (SkullMeta) skull.getItemMeta();
-			im.setOwner(p.getName());
-			skull.setItemMeta(im);
-			a.setHelmet(skull);
-			health = p.getHealth();
-			z = p.getWorld().spawn(p.getLocation(), Zombie.class);
-			
-			z.setBaby(true);
-			z.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY,600000,1));
-			z.getEquipment().setHelmet(null);
-			z.getEquipment().setChestplate(null);
-			z.getEquipment().setLeggings(null);
-			z.getEquipment().setBoots(null);
-			z.getEquipment().setItemInHand(null);
-			new BukkitRunnable() {
-				
-				@Override
-				public void run() {
-				
-				a.setRightArmPose(new EulerAngle(0,0,0));
-				a.setLeftArmPose(new EulerAngle(0,0,0));
-				Location loc = z.getLocation();
-				loc.add(1, -1, 0);
-				a.setVelocity(z.getEyeLocation().getDirection());
-				a.teleport(loc);
-				a.setRightArmPose(new EulerAngle(1,0,0));
-				a.setLeftArmPose(new EulerAngle(1,0,0));
-					
-				}
-			}.runTaskTimer(this, 2, 2);
-	
-			
-			
-			
-			
-			return true;
-		}
-      
-		return false;
-
 	}
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent e){
+    	if(e.getEntityType() == EntityType.ZOMBIE) {
+    		Zombie z = (Zombie) e.getEntity();
+    		z.setHealth(0);
+    		SpawnMonster();
+    	}
+    	
+    }
+	private void SpawnMonster() {
+		Location l = p.getLocation();
+		l.add(0, -1, 0);
+		final ArmorStand a = p.getWorld().spawn(l, ArmorStand.class);
+		a.setGravity(false);
+	
+		a.setBodyPose(new EulerAngle(1,0,0));
+		a.setArms(true);
+		a.setVisible(false);
+		a.setChestplate(new ItemStack(Material.CHAINMAIL_CHESTPLATE));
+		ItemStack skull = new ItemStack(Material.SKULL_ITEM,1,(short)3);
+		SkullMeta im = (SkullMeta) skull.getItemMeta();
+		im.setOwner(p.getName());
+		skull.setItemMeta(im);
+		a.setHelmet(skull);
+		health = p.getHealth();
+		z = p.getWorld().spawn(p.getLocation(), Zombie.class);
+		
+		z.setBaby(true);
+		z.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY,600000,1));
+		z.getEquipment().setHelmet(null);
+		z.getEquipment().setChestplate(null);
+		z.getEquipment().setLeggings(null);
+		z.getEquipment().setBoots(null);
+		z.getEquipment().setItemInHand(null);
+		new BukkitRunnable() {
+			
+			@Override
+			public void run() {
+			
+			a.setRightArmPose(new EulerAngle(0,0,0));
+			a.setLeftArmPose(new EulerAngle(0,0,0));
+			Location loc = z.getLocation();
+			loc.add(1, -1, 0);
+			a.setVelocity(z.getEyeLocation().getDirection());
+			a.teleport(loc);
+			a.setRightArmPose(new EulerAngle(1,0,0));
+			a.setLeftArmPose(new EulerAngle(1,0,0));
+				
+			}
+		}.runTaskTimer(this, 2, 2);
+		
+	}
+	
+
 	final boolean isTargeting = z.getTarget() == p && p.getHealth() < health;
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e){
---
+++ EspadaCheta/src/plugin/EspadaChetaPlugin.java
@@ -0,0 +1,58 @@
+package plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Pig;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+public class EspadaChetaPlugin extends JavaPlugin implements Listener {
+	private boolean activo = false;
+
+	@Override
+	public void onEnable() {
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,String label, String[] args) {
+		if(!(sender instanceof Player))
+		 NO CERRAR!!!!!!!!!!!!!!
+		 
+		
+		if(command.getName().equalsIgnoreCase("espadacheta")){
+			 
+			
+			
+			
+			
+			
+			
+		}
+		return false;
+	}
+
+}
+
+
+
+
+
+
